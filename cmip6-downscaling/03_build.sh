f1996bcc99e677c648359b2c34b7b9c64b639bb4
Joseph Hamman <jhamman@ucar.edu>
2022-04-08 14:32:13 -0700

    update cmip6 env

--- cmip6-downscaling/03_build.sh
+++ cmip6-downscaling/03_build.sh
@@ -3,7 +3,7 @@
 set -x
 set -e
 
-for dir in dask-worker prefect single-user; do
+for dir in single-user prefect dask-worker; do
     echo $dir
     cd $dir
     docker build . -t carbonplan/cmip6-downscaling-$dir:latest
