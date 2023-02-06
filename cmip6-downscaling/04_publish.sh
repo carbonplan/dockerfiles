f853816637f054dcc378dcb9856c7f3ae8f27a28
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-02-06 12:22:08 -0700

    add publish step

--- cmip6-downscaling/04_publish.sh
+++ cmip6-downscaling/04_publish.sh
@@ -6,7 +6,7 @@ set -e
 for dir in prefect single-user; do
     echo $dir
 
-    docker tag carbonplan/cmip6-downscaling-$dir:latest carbonplan/cmip6-downscaling-$dir:$(date +"%Y.%m.%d")
+    docker tag carbonplan/cmip6-downscaling-$dir:latest carbonplan/cmip6-downscaling-$dir:"$(date +"%Y.%m.%d")"
     docker push carbonplan/cmip6-downscaling-$dir:latest
-    docker push carbonplan/cmip6-downscaling-$dir:$(date +"%Y.%m.%d")
+    docker push carbonplan/cmip6-downscaling-$dir:"$(date +"%Y.%m.%d")"
 done
--- fire-monitor/04_publish.sh
+++ fire-monitor/04_publish.sh
@@ -6,7 +6,7 @@ set -e
 for dir in prefect single-user; do
     echo $dir
 
-    docker tag carbonplan/fire-monitor-$dir:latest carbonplan/fire-monitor-$dir:$(date +"%Y.%m.%d")
+    docker tag carbonplan/fire-monitor-$dir:latest carbonplan/fire-monitor-$dir:"$(date +"%Y.%m.%d")"
     docker push carbonplan/fire-monitor-$dir:latest
-    docker push carbonplan/fire-monitor-$dir:$(date +"%Y.%m.%d")
+    docker push carbonplan/fire-monitor-$dir:"$(date +"%Y.%m.%d")"
 done
