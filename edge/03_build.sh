65d71a30ccb80e55acb1efa21010773cfcda1144
Raphael Hagen <norlandrhagen@gmail.com>
2023-02-06 16:53:12 -0700

    updated 03_build.sh

--- edge/03_build.sh
+++ edge/03_build.sh
@@ -6,6 +6,9 @@ set -e
 for dir in single-user; do
     echo $dir
     cd $dir
-    docker build . -t carbonplan/main-$dir:latest
+    docker build . -t carbonplan/edge-$dir:latest
+    docker build . -t carbonplan/edge-$dir:"$(date +"%Y.%m.%d")"
+
     cd ..
 done
+
