dc68e3bc17686eb708733063ea96a6de08765979
Raphael Hagen <norlandrhagen@gmail.com>
2023-02-06 16:38:49 -0700

    updated edge publish

--- edge/04_publish.sh
+++ edge/04_publish.sh
@@ -5,5 +5,7 @@ set -e
 
 for dir in single-user; do
     echo $dir
-    docker push carbonplan/main-$dir:latest
+    docker push carbonplan/edge-$dir:latest
+    docker push carbonplan/edge-$dir:"$(date +"%Y.%m.%d")"
+
 done
