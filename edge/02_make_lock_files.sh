65708126786401ebd9a83f05e3cf11bd4ccf595e
Raphael Hagen <norlandrhagen@gmail.com>
2023-03-22 16:20:47 -0700

    removed macos version

--- edge/02_make_lock_files.sh
+++ edge/02_make_lock_files.sh
@@ -7,11 +7,10 @@ conda-lock --version
 conda --version
 conda-lock \
     -f conda-envs/base.yaml \
-    -p osx-64 -p linux-64 \
+    -p linux-64 \
     --filename-template "single-user/conda-{platform}.lock"
 
 
 for dir in single-user; do
     ../list_packages.sh $dir/conda-linux-64.lock | sort > $dir/linux-64-packages.txt
-    ../list_packages.sh $dir/conda-osx-64.lock | sort > $dir/osx-64-packages.txt
 done
