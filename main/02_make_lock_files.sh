ed590cb3de1e547de3343dba5a84da9dd191fe2b
Raphael Hagen <norlandrhagen@gmail.com>
2023-03-29 13:06:22 -0700

    removed osx from main single user

--- main/02_make_lock_files.sh
+++ main/02_make_lock_files.sh
@@ -9,7 +9,7 @@ conda-lock \
     -f conda-envs/jupyterlab.yaml \
     -f conda-envs/analysis.yaml \
     -f conda-envs/dask.yaml \
-    -p osx-64 -p linux-64 \
+    -p linux-64 \
     --filename-template "single-user/conda-{platform}.lock"
 
 echo "dask-worker"
@@ -19,7 +19,7 @@ conda-lock \
     -f conda-envs/base.yaml \
     -f conda-envs/analysis.yaml \
     -f conda-envs/dask.yaml \
-    -p osx-64 -p linux-64 \
+    -p linux-64 \
     --filename-template "dask-worker/conda-{platform}.lock"
 
 echo "prefect"
@@ -28,10 +28,9 @@ conda --version
 conda-lock \
     -f conda-envs/base.yaml \
     -f conda-envs/prefect.yaml \
-    -p osx-64 -p linux-64 \
+    -p linux-64 \
     --filename-template "prefect/conda-{platform}.lock"
 
 for dir in dask-worker prefect single-user; do
     ../list_packages.sh $dir/conda-linux-64.lock | sort > $dir/linux-64-packages.txt
-    ../list_packages.sh $dir/conda-osx-64.lock | sort > $dir/osx-64-packages.txt
 done
