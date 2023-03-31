b457f5d136c3928005cd1c0d0b08e2fa42e15093
Raphael Hagen <norlandrhagen@gmail.com>
2023-03-31 10:54:45 -0700

    simplified edge

--- edge/02_make_lock_files.sh
+++ edge/02_make_lock_files.sh
@@ -8,9 +8,4 @@ conda --version
 conda-lock \
     -f conda-envs/base.yaml \
     -p linux-64 \
-    --filename-template "single-user/conda-{platform}.lock"
-
-
-for dir in single-user; do
-    ../list_packages.sh $dir/conda-linux-64.lock | sort > $dir/linux-64-packages.txt
-done
+    --lockfile "single-user/conda-linux-64.lock"
