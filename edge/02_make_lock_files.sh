dcd247f323d7126fa40160c05a9335ea5d84d6c3
Raphael Hagen <norlandrhagen@gmail.com>
2023-04-07 13:59:28 -0700

    reverted build and added -k explicit arg

--- edge/02_make_lock_files.sh
+++ edge/02_make_lock_files.sh
@@ -8,4 +8,11 @@ conda --version
 conda-lock \
     -f conda-envs/base.yaml \
     -p linux-64 \
-    --lockfile "single-user/conda-linux-64.lock"
+    -k explicit \
+    --filename-template "single-user/conda-linux-64.lock"
+
+
+
+for dir in single-user; do
+    ../list_packages.sh $dir/conda-linux-64.lock | sort > $dir/linux-64-packages.txt
+done
