b41deedddd301b4de00035888cb4e157a889dd64
Raphael Hagen <norlandrhagen@gmail.com>
2023-05-17 17:43:01 -0600

    removed notebook test

--- image-tests/test_import.py
+++ image-tests/test_import.py
@@ -2,6 +2,5 @@
 # This will try to import carbonplan-styles which is a packages that
 # should be in our yml, but not be in the Pangeo community image.
 
-
 def test_import_carbonplan_styles():
   import carbonplan_styles
