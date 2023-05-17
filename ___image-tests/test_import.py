0ebf3aad833c50c981360a6ec851d598ea6c5e84
Raphael Hagen <norlandrhagen@gmail.com>
2023-05-17 17:17:14 -0600

    nit

--- ___image-tests/test_import.py
+++
@@ -1,7 +0,0 @@
-
-# This will try to import carbonplan-styles which is a packages that
-# should be in our yml, but not be in the Pangeo community image.
-
-
-def test_import_carbonplan_styles():
-  import carbonplan_styles
